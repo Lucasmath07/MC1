6fa5e5b4e4aa00c888b8cdd244cd5574e5207773
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-06-28 19:14:06 -0300

    Update cifra.cpp

--- cifra.cpp
+++ cifra.cpp
@@ -11,31 +11,26 @@ string Cifra(string frase, int chave) {
         if(letraAtual == ' '){  //mantem o espaço sem alteração
             continue;
         }
-        // Letras maiúsculas
-        if(letraAtual + chave < 'Z'){
-            frase[i] = letraAtual + chave;
-        }
-        if(letraAtual + chave > 'Z'){
+        if (letraAtual >= 'A' && letraAtual <= 'Z') { //cifra para letra maiuscula
+            
             frase[i] = (letraAtual - 'A' + chave) % 26 + 'A';
         }
-        if(letraAtual + chave < 'z'){
-            frase[i] = letraAtual + chave;
-        }
-        if(letraAtual + chave > 'z'){
+        else if (letraAtual >= 'a' && letraAtual <= 'z') { //cifra para letras minusculas
+
             frase[i] = (letraAtual - 'a' + chave) % 26 + 'a';
         }
-
-       
+        
     }
+    
     return frase;
 }
 
 int main() {
     int chave;
     string frase;
-    cout << "Digite a mensagem que deseja cifrar:\n";
+    cout << "Digite a mensagem que deseja cifrar (letras maiusculas e minusculas):\n";
     getline(cin, frase);
-    cout << "Digite a chave da cifra:\n";
+    cout << "Digite a chave da cifra (entre 0 e 26):\n";
     cin >> chave;
 
     if (chave < 0) {

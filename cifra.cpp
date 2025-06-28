ab7e9102441538689b5e98bac4bc363d570e32da
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-06-28 18:44:27 -0300

    Update cifra.cpp

--- cifra.cpp
+++ cifra.cpp
@@ -2,61 +2,53 @@
 #include <string>
 using namespace std;
 
-string Cifra(string a, int chave) {
-    int tam = a.size();
-    for (int i = 0; i < tam; i++) {
-        char c = a[i];
+string Cifra(string frase, int chave) {
+    int tamanhoDaFrase = frase.size();
+    for (int i = 0; i < tamanhoDaFrase; i++) {
+        char letraAtual = frase[i];
 
         
         // Mantém caracteres especiais de 33 a 47 inalterados
         /*if (c >= '!' && c <= '/') {
             continue;
         }*/
-        if(c == ' '){
+        if(letraAtual == ' '){
             continue;
         }
         // Letras maiúsculas
-        if(c+chave < 'Z'){
-            a[i]=c+chave;
+        if(letraAtual + chave < 'Z'){
+            frase[i] = letraAtual + chave;
         }
-        if(c+chave > 'Z'){
-            a[i]=(c-'A'+chave)%26+'A';
+        if(letraAtual + chave > 'Z'){
+            frase[i] = (letraAtual - 'A' + chave) % 26 + 'A';
         }
+        if(letraAtual + chave < 'z'){
+            frase[i] = letraAtual + chave;
+        }
+        if(letraAtual + chave > 'z'){
+            frase[i] = (letraAtual - 'a' + chave) % 26 + 'a';
+        }
+
        
-        
-        //if (c >= 'A' && c <= 'Z') {
-          //  a[i] = (c + chave) % 26;
-        //}
-
-        // Letras minúsculas
-        /*else if (c >= 'a' && c <= 'z') {
-            a[i] = (c - 'a' + chave) % 26 + 'a';
-        }
-
-        // Números
-        else if (c >= '0' && c <= '9') {
-            a[i] = (c - '0' + chave) % 10 + '0';
-        }*/
-
-        // Outros caracteres permanecem inalterados (inclusive espaços, acentos, etc.)
     }
-    return a;
+    return frase;
 }
 
 int main() {
     int chave;
-    string a;
+    string frase;
     cout << "Digite a mensagem que deseja cifrar:\n";
-    getline(cin, a);
-    cout << "Digite a chave da cifra (positiva):\n";
+    getline(cin, frase);
+    cout << "Digite a chave da cifra:\n";
     cin >> chave;
 
     if (chave < 0) {
         cout << "A chave deve ser positiva.\n";
-        return 1;
+        return 0;
     }
 
-    cout << "A mensagem cifrada é: " << Cifra(a, chave) << endl;
+    cout << "A mensagem cifrada eh: " << Cifra(frase, chave) << endl;
 
     return 0;
 }
+

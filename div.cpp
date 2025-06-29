266b9dd992bffd51765e303a06a6ff6a3f93127b
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-06-29 11:21:27 -0300

    Update div.cpp

--- div.cpp
+++ div.cpp
@@ -2,51 +2,38 @@
 
 using namespace std;
 
-int divisao(int dividendo,int divisor){
+int div_alg(int a, int b){
+    int cont=0;
     
-        if((dividendo >= 0 && divisor >0) ){
-            for(int c = 1;c<=dividendo;c++){
-                if(divisor*c==dividendo){
-                    return c;
-                }
-        
-            }
-        }
-        if((dividendo < 0 && divisor <0) ){
-            for(int c = 1;c<=dividendo*-1;c++){
-                if(divisor*c==dividendo){
-                    return c;
-                }
-        
-            }
-        }
-        if(dividendo < 0 && divisor > 0){
-            for(int c = -1;c>=dividendo;c--){
-                if(divisor*c==dividendo){
-                    return c;
-                }
-            }
-        }   
-         if(dividendo > 0 && divisor < 0){
-            for(int c = -1;c>=dividendo*-1;c--){
-                if(divisor*c==dividendo){
-                    return c;
-                }
-            }
-        }
-    return 0;
+    if(a<0){ a = -a; cont++;}
+    if(b<0){ b = -b; cont++;}
     
-}
+    int resto = a;
+    int quociente = 0;
 
-int main(){
-    int divisor,dividendo;
-    cin>>dividendo;
-    cin>>divisor;
-    if(divisor != 0){
-    cout<<divisao(dividendo,divisor)<<endl;
+    while(resto >= b){
+        quociente++;
+        resto = resto - b;
+    }
+    if(cont != 1){
+        return quociente;
     }
     else{
-        cout<<"Nao e possivel dividir por zero! \n";
+        quociente = -quociente;
+        return quociente;
+    }
+}
+int main(){
+    int a,b;
+    cout << "Digite o dividendo: ";
+    cin >> a;
+    cout << "Digite o divisor: ";
+    cin >> b;
+    if(b != 0){
+        cout << "Resultado da divisao: " << div_alg(a,b);
+    }
+    else{
+        cout << "A divisao por zero eh indefinida use outro divisor.\n";
     }
     return 0;
 }

5ffff8b8227cc50ca1a7b62859ed4a3c56ce36d6
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-06-29 12:19:27 -0300

    Update teto (1).cpp

--- teto (1).cpp
+++ teto (1).cpp
@@ -1,37 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-int Teto(float alfa){
+int Teto(float alfa) {
     int teto = 0;
 
-    if(alfa>0){
-        for(int i = alfa;i>=0;i--){
-            ++teto;
-            //cout<<teto<<endl;
-            if(teto == alfa){
-                return teto;
-            }
-        }
-    }
-    if(alfa<0){
-        for(int i = alfa;i<0;i++){
-            teto--;
-            if(teto == alfa){
-                return teto;
-            }
-            
+    if (alfa > 0) {
+        while (teto < alfa) {
+            teto++;
         }
         return teto;
+    } else {
+        while (teto >= alfa) {
+            teto--;
+        }
+        return teto + 1;
+        
     }
-    
-    return teto;
 }
-int main(){
-    float a;
-    cout<<"Digite o numero para implementar a funcao teto: ";
-    cin>>a;
-    cout<<"O teto de "<<a<<" eh: "<<Teto(a);
-    return 0;
 
+int main() {
+    float a;
+    cout << "Digite o numero para implementar a funcao teto: ";
+    cin >> a;
+    cout << "O teto de " << a << " eh: " << Teto(a);
+    return 0;
 }

f34b45f941f914dbb0fefaa3500a009c522143f1
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-06-29 11:51:09 -0300

    Update piso.cpp

--- piso.cpp
+++ piso.cpp
@@ -2,22 +2,31 @@
 
 using namespace std;
 
-int Piso(float alfa) {
+int Piso(float alfa){
     int piso = 0;
 
-    if (alfa >= 0) {
-        while (piso <= alfa) {
+    if(alfa>0){
+        for(int i = alfa;i>0;i--){
             piso++;
-        }
-        return piso - 1;
-    } else {
-        while (piso > alfa) {
-            piso--;
+            
+            if(piso == alfa){
+                return piso;
+            }
         }
         return piso;
     }
+    if(alfa<0){
+        for(float i = alfa;i<=0;i++){
+            piso--;
+            if(piso == alfa){
+                return piso;
+            }
+            
+        }
+        return piso;
+    }
+    
 }
-
 int main(){
     float a;
     cout<<"Digite o numero para implementar a funcao piso: ";

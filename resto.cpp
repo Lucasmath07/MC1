f74c4dd550b817724ed08ec5a0a35e004a59c4bf
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-06-29 16:26:29 -0300

    Update resto.cpp

--- resto.cpp
+++ resto.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
-
 using namespace std;
 
-int resto(int dividendo, int divisor){
-    
-    int res;
-    
-    if(dividendo < 0){dividendo = dividendo*(-1);}//deixa o dividendo positivo
-    if(divisor < 0){divisor = divisor*(-1);}//deixa o divisor positivo
 
-    int divi_dendo = dividendo; //variavel auxiliar para ajudar na divisao
-        for(int i = 0;i<divi_dendo/2;i++){
-            divi_dendo = divi_dendo - divisor; //usa a definição de divisão a|b se Exite um c t.q. a*c=b.
-            if(divi_dendo<divisor){//o resto é obtido quando o dividendo é menor que o divisor
-                return divi_dendo;
-                break;
-            }
-        }
-    
-     
-  return 0;
+int mod(int a, int b) {
+
+    if (b < 0) { //por definição no calculo do mod sempre usamos |b| t.q. 0 <= r < |b|
+        b = -b;
+    }
+
+    int resto = a;
+
+    while(resto < 0) {
+        resto = resto + b; //se o "a" for negativo, somamos "b" até resto ser maior que zero
+    }
+
+    while(resto >= b) { // caso o resto seja maior ou igual a b, subtraimos b do resto até que o resto seja menor que b
+        resto = resto - b;
+    }
+
+    return resto;
 }
-int main(){
-    int dividendo;
-    int divisor;
-    cout<<"Digite o dividendo:";
-    cin>>dividendo;
-    cout<<"Digite o divisor:";
-    cin>>divisor;
-    
-    cout<<resto(dividendo, divisor)<<endl;
-    
+
+int main() {
+    int a, b;
+    cout << "Digite o dividendo: ";
+    cin >> a;
+    cout << "Digite o divisor: ";
+    cin >> b;
+    if (b == 0) {
+        cout << "Divisao por zero eh indefinida. Digite outro valor para o denominador" << endl;
+        return 0;
+    }
+    cout << a << " mod " << b <<" = "<< mod(a, b) << endl;
+
     return 0;
 }
+
